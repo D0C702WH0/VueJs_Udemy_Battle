7f9861cced20592c071d71c0d5f2c36ba18bc0bf
Jeremy_P <jemyplu@gmail.com>
2020-02-12 11:21:01 +0100

    attack methods

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 new Vue({
-    el: "#app",
-    data: {
-        playerHealth: 100,
-        monsterHealth: 100,
-        gamisIsRunning: false
+  el: "#app",
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gamisIsRunning: false
+  },
+  methods: {
+    startGame: function () {
+      this.gamisIsRunning = true
+      this.playerHealth = 100
+      this.monsterHealth = 100
     },
-    methods: {
-        startGame: function () {
-           this.gamisIsRunning = true 
-        }
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10)
+
+      if (this.monsterHealth <= 0) {
+        alert('You won')
+        return this.gamisIsRunning = false
+      }
+
+      this.playerHealth -=  this.calculateDamage(5, 12)
+
+      if (this.playerHealth <= 0) {
+        alert('You Lost')
+        return this.gamisIsRunning = false
+      }
+    },
+    spAttack: function () {
+      
+    },
+    heal: function () {
+      
+    },
+    giveUp: function () {
+      
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
     }
+  }
 })
